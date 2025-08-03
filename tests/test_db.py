d4fa5d7a7fc3ce8728c917c35af19adc7f8215ba
wanessa <wanessaldantas@gmail.com>
2025-08-03 00:16:59 -0300

    Refatora models, configurações e testes

--- tests/test_db.py
+++ tests/test_db.py
@@ -6,14 +6,16 @@ from fastapi_zero.models import User
 
 
 def test_create_user(session, mock_db_time):
+
     with mock_db_time(model=User) as time:
+
         new_user = User(username='test', email='test@test', password='secret')
 
         # pattern: unit of work
         # Realiza várias op na sessão e uma única op com o db
         session.add(new_user)
         session.commit()
-
+        
         user = session.scalar(select(User).where(User.username == 'test'))
 
     assert asdict(user) == {
@@ -22,4 +24,5 @@ def test_create_user(session, mock_db_time):
         'email': 'test@test',
         'password': 'secret',
         'created_at': time,
+        'updated_at': time,
     }

c9c270625433f89f8d64c05e52ff7bcd674ad6f5
wanessa <wanessaldantas@gmail.com>
2025-12-08 09:10:20 -0300

    Refatorando teste de integridade para receber other_user

--- tests/test_users.py
+++ tests/test_users.py
@@ -90,7 +90,7 @@ def test_delete_user_wrong_user(client, other_user, token):
     assert response.json() == {'detail': 'Not enough permissions'}
 
 
-def test_update_integrity_error(client, user, token):
+def test_update_integrity_error(client, user, other_user, token):
     # Criando um registro para "fausto"
     client.post(
         '/users',
@@ -107,7 +107,7 @@ def test_update_integrity_error(client, user, token):
         f'/users/{user.id}',
         headers={'Authorization': f'Bearer {token}'},
         json={
-            'username': 'fausto',
+            'username': other_user.username,
             'email': 'bob@example.com',
             'password': 'mynewpassword',
         },
